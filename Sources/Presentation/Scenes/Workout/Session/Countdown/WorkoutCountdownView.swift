bb5b24464de8de4198f37426cb9effe9651f0a1b
Frédéric Maquin <fred@ephread.com>
2024-01-09 02:38:44 -0500

    feature: add a better-looking countdown

--- Sources/Presentation/Scenes/Workout/Session/Countdown/WorkoutCountdownView.swift
+++ Sources/Presentation/Scenes/Workout/Session/Countdown/WorkoutCountdownView.swift
@@ -6,108 +6,203 @@
 import SwiftUI
 import Resolver
 
+// TODO: Document & Use constants for some animation durations.
+
+// MARK: Private Types
+
+private struct StrokeAnimationProperties {
+    var progress: CGFloat = 0
+    var lineWidth: CGFloat = 0
+}
+
+private struct TextAnimationProperties {
+    var opacity: CGFloat = 0
+    var scale = CGSize(width: 0.2, height: 0.2)
+}
+
+private enum CountdownPhase: String, Hashable, CaseIterable {
+    case ready, three, two, one
+
+    var label: String {
+        return switch self {
+        case .ready: "Ready"
+        case .three: "3"
+        case .two: "2"
+        case .one: "1"
+        }
+    }
+
+    var fontSize: CGFloat {
+        return switch self {
+        case .ready: 30
+        default: 60
+        }
+    }
+
+    var delay: TimeInterval {
+        return switch self {
+        case .ready: 0.8
+        case .three: 2.5
+        case .two: 4.0
+        case .one: 5.5
+        }
+    }
+}
+
 // MARK: - Views
+
+struct CountdownStroke: View {
+
+    let trigger: Bool
+
+    var body: some View {
+        KeyframeAnimator(
+            initialValue: StrokeAnimationProperties(),
+            trigger: trigger
+        ) { value in
+            ZStack {
+                Circle()
+                    .stroke(
+                        LinearGradient(
+                            colors: [Color(.gradientTop), Color(.gradientBottom)],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        style: StrokeStyle(
+                            lineWidth: value.lineWidth,
+                            lineCap: .round,
+                            lineJoin: .round
+                        )
+                    )
+                    .foregroundColor(.gray)
+                    .opacity(0.3)
+
+                Circle()
+                    .trim(from: 0.0, to: min(value.progress, 1.0))
+                    .stroke(
+                        LinearGradient(
+                            colors: [Color(.gradientTop), Color(.gradientBottom)],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        style: StrokeStyle(
+                            lineWidth: value.lineWidth,
+                            lineCap: .round,
+                            lineJoin: .round
+                        )
+                    )
+                    .rotationEffect(.degrees(-90))
+            }
+        } keyframes: { _ in
+            KeyframeTrack(\.progress) {
+                LinearKeyframe(0.0, duration: 0.7)
+
+                CubicKeyframe(1.0, duration: 1.0)
+                LinearKeyframe(1.0, duration: 0.7)
+
+                CubicKeyframe(0.66, duration: 1.0)
+                LinearKeyframe(0.66, duration: 0.5)
+
+                CubicKeyframe(0.33, duration: 1.0)
+                LinearKeyframe(0.33, duration: 0.5)
+
+                CubicKeyframe(0.001, duration: 1.0)
+                LinearKeyframe(0.001, duration: 0.5)
+
+                LinearKeyframe(0.001, duration: 0.3)
+            }
+
+            KeyframeTrack(\.lineWidth) {
+                SpringKeyframe(15, duration: 0.3)
+                LinearKeyframe(15, duration: 6.6)
+                SpringKeyframe(0, duration: 0.3)
+                MoveKeyframe(0)
+            }
+        }
+    }
+}
+
+struct CountdownText: View {
+
+    let trigger: Bool
+
+    var body: some View {
+        ZStack {
+            ForEach(CountdownPhase.allCases, id: \.self) { phase in
+                Text(phase.label)
+                    .font(Font.system(size: phase.fontSize))
+                    .fontDesign(.rounded)
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+                    .keyframeAnimator(
+                        initialValue: TextAnimationProperties(),
+                        trigger: trigger
+                    ) { view, value in
+                        view
+                            .opacity(value.opacity)
+                            .scaleEffect(value.scale)
+                    } keyframes: { _ in
+                        KeyframeTrack(\.opacity) {
+                            LinearKeyframe(0.0, duration: phase.delay)
+                            SpringKeyframe(1.0, duration: 0.5)
+                            LinearKeyframe(1.0, duration: 1.0)
+                            SpringKeyframe(0.0, duration: 0.5)
+                            MoveKeyframe(0)
+                        }
+
+                        KeyframeTrack(\.scale) {
+                            SpringKeyframe(CGSize(width: 0.2, height: 0.2), duration: phase.delay)
+                            SpringKeyframe(CGSize(width: 1.0, height: 1.0), duration: 0.5)
+                            LinearKeyframe(CGSize(width: 1.0, height: 1.0), duration: 1)
+                            SpringKeyframe(CGSize(width: 0.2, height: 0.2), duration: 0.5)
+                        }
+                    }
+            }
+        }
+    }
+}
 
 /// Displays a countdown before starting the workout.
 struct WorkoutCountdownView: View {
     // MARK: Bindings
     @ObservedObject var viewModel: WorkoutViewModel
 
-    // MARK: Private State
-    /// The current step of the countdown, see ``AnimationStep``
-    /// for more information.
-    @State private var step: AnimationStep = .empty
-
-    /// Whether the label is visible. This property is updated
-    /// during the countdown animation.
-    @State private var isLabelVisible = false
-
-    // MARK: Private Properties
-    private var countdownLabel: String {
-        switch step {
-        case .ready: return "Ready"
-        case .three: return "3"
-        case .two: return "2"
-        case .one: return "1"
-        case .empty: return ""
-        }
-    }
+    @State private var isAnimating = false
 
     // MARK: Body
     var body: some View {
-        Text(countdownLabel)
-            .opacity(isLabelVisible ? 1 : 0)
-            .font(.title)
-            .fontDesign(.rounded)
-            .fontWeight(.semibold)
-            .onAppear {
+        ZStack {
+            CountdownStroke(trigger: isAnimating)
+            CountdownText(trigger: isAnimating)
+        }
+        .task {
+            Task.delayed(seconds: 0.2) { @MainActor in
                 if viewModel.currentScene == .countdown {
-                    animateViews()
-                }
-            }
-    }
-
-    // MARK: Private Methods
-
-    // TODO: Revamp animation.
-    // The animation uses old dispatches to animate, for a lack of better options.
-    // This is old code which could be modernized. An animation using a progress gauge
-    // would be nicer instead.
-    private func animateViews() {
-
-        let now: DispatchTime = .now()
-
-        for (index, step) in zip(AnimationStep.allCases.indices, AnimationStep.allCases) {
-            DispatchQueue.main.asyncAfter(deadline: fadeInDeadline(from: now, index: index)) {
-                self.step = step
-                withAnimation(.easeIn(duration: 0.2)) {
-                    isLabelVisible = true
+                    isAnimating = true
                 }
             }
 
-            DispatchQueue.main.asyncAfter(deadline: fadeOutDeadline(from: now, index: index)) {
-                withAnimation(.easeIn(duration: 0.4)) {
-                    isLabelVisible = false
-                }
+            Task.delayed(seconds: 7.1) { @MainActor in
+                await viewModel.didCompleteCountdown()
             }
         }
-
-        DispatchQueue.main.asyncAfter(deadline: dismissDeadline(from: now)) {
-            Task { await viewModel.didCompleteCountdown() }
-        }
-    }
-
-    private func fadeInDeadline(from now: DispatchTime, index: Int) -> DispatchTime {
-        now + 0.3 + 1.0 * CGFloat(index)
-    }
-
-    private func fadeOutDeadline(from now: DispatchTime, index: Int) -> DispatchTime {
-        now + 0.8 + 1.0 * CGFloat(index)
-    }
-
-    private func dismissDeadline(from now: DispatchTime) -> DispatchTime {
-        now + CGFloat(AnimationStep.allCases.count - 1) * 1.0 + 1.0
-    }
-
-    // MARK: Private Types
-    enum AnimationStep: CaseIterable {
-        case ready, three, two, one, empty
     }
 }
 
 #if DEBUG
 
 // MARK: - Previews
-struct WorkoutCountdownView_Previews: PreviewProvider {
-    @StateObject static var viewModel = WorkoutViewModelPreview(
+struct WorkoutCountdownViewPreview: View {
+    @StateObject private var viewModel = WorkoutViewModelPreview(
         mainViewModel: Resolver.resolve(),
         errorViewModel: Resolver.resolve(),
         workoutManager: Resolver.resolve()
     )
 
-    @State static var isPresented = false
+    @State private var isPresented = false
 
-    static var previews: some View {
+    var body: some View {
         VStack {
             Button {
                 viewModel.startCountdown()
@@ -120,6 +215,10 @@ struct WorkoutCountdownView_Previews: PreviewProvider {
             WorkoutCountdownView(viewModel: viewModel)
         }
     }
+}
+
+#Preview {
+    WorkoutCountdownViewPreview()
 }
 
 #endif
